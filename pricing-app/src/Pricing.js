80631733c7368d05cca07a582d20e45c8dc9035e
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2021-02-01 12:28:59 -0600

    Estilos de Background

--- pricing-app/src/Pricing.js
+++ pricing-app/src/Pricing.js
@@ -1,9 +1,23 @@
+import { Top } from "./components/svg/Top";
+
 function Pricing() {
+
   return (
+
     <div className="pricing__container">
-      <h1>Hola Mundo</h1>
+      <div className="pricing__topl">
+        <Top />
+      </div>
+      <div className="pricing__main">
+        Hola Mundo
+      </div>
+      <div className="pricing__topr">
+        <Top />
+      </div>
     </div>
+
   );
+  
 }
 
 export default Pricing;
--- pricing-app/src/index.js
+++ pricing-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pricing from './Pricing';
+import './styles/styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
